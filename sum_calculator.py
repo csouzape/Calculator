8c4c5affa3f78de5fdfd0de9d29d7fd03e4751f8
csouzape <oditorrinco222@gmail.com>
2025-07-10 16:46:49 -0300

    added content

--- sum_calculator.py
+++ sum_calculator.py
@@ -1,1 +1,14 @@
-print("Hello-World")
+numero1 = float(input("Digite o seu primeiro numero: "))
+numero2 = float(input("Digite o seu segundo numero:"))
+operacao = input("Qual operacao voce quer?: +, -, *, /):")
+
+if operacao == "+":
+    print("Soma:", numero1 + numero2) 
+elif operacao == "-":
+    print("Subtração:", numero1 - numero2)
+elif operacao == "*":
+    print("Multiplicação:", numero1 * numero2) 
+elif operacao == "/":
+    print("Divisão", numero1 / numero2)
+else:
+    print("Operação Invalida")
